d57bea3e30b793388656408ae783ffcf83d8ff9e
unknown <will.ediger@gmail.com>
2019-03-27 22:21:38 -0500

    implemented movie selector mvp

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 
+import { Route } from 'react-router-dom';
+
 export default class App extends Component {
   constructor() {
     super();
@@ -22,7 +24,10 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        {/* <Route path="/" component={Navigation} /> */}
+        <Route path="/" exact component={MovieList } />
+        <Route path="/movies/:id" exact component={Movie} />
+        {/* <Route path="/contact" exact component={Contact} /> */}
       </div>
     );
   }
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import {Link} from 'react-router-dom'
+
 export default class SavedList extends Component {
   constructor(props) {
     super(props);
@@ -12,7 +14,7 @@ export default class SavedList extends Component {
         {this.props.list.map(movie => (
           <span className="saved-movie">{movie.title}</span>
         ))}
-        <div className="home-button">Home</div>
+        <Link to="/"><div className="home-button">Home</div></Link>
       </div>
     );
   }
